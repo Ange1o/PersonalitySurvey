5dd8e66c7141a1f4ee0c9b9a007f7ed021dd2852
Angelo <icream92@163.com>
2016-11-29 03:28:09 +0000

    finished check

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,42 @@ var didClickIt = false;
             if( didClickIt ) {
                 didClickIt = false;
                 // document.write causes silly problems, do this instead (or better yet, use a library like jQuery to do this stuff for you)
-                var o=document.getElementById("output"); 
-                var b = document.getElementById("p1"); 
+                
 
-if (document.getElementById('q1y').checked) {
-  vv = document.getElementById('q1y');
+
+var finished = true;
+var result = [];
+
+
+// need change to 41
+var temp = 5;
+
+
+for (var i = 1; i <temp; i++) {
+    if (document.getElementById('q'+i+'y').checked) {
+        result[i] = document.getElementById('q'+i+'y').value;
+    } else if(document.getElementById('q'+i+'n').checked){
+        result[i] = document.getElementById('q'+i+'n').value;
+    } else if(document.getElementById('q'+i+'yn').checked){
+        result[i] = document.getElementById('q'+i+'yn').value;
+    } else{
+        finished = false;
+        break;
+    }
 }
 
 
+var o=document.getElementById("output"); 
+                var b = document.getElementById("p1"); 
+var e = document.getElementById("error"); 
 
+if (finished){
 
-                b.innerHTML = "2";
                o.style.display='block';
+}else{
+e.style.display = 'block';
+}
+
 
                 
             }
